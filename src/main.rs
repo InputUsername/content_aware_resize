23ca4b697dc3eb7a453d991c6889dab8fad34efb
Koen Bolhuis <koen.bolhuis@gmail.com>
2019-06-27 23:08:57 +0200

    Convert image into image::RgbImage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::env;
 
+use image::DynamicImage;
+
 fn main() {
     let file = env::args().nth(1)
         .expect("Expected a file");
     let img = image::open(file).expect("Failed to open image");
+
+    let img_buf = if let DynamicImage::ImageRgb8(buf) = img {
+        buf
+    } else {
+        img.to_rgb()
+    };
 }
