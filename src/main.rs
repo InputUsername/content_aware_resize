4fa89464cbdb326a50517d614847af4e3cb28071
Koen Bolhuis <koen.bolhuis@gmail.com>
2019-07-02 23:43:22 +0200

    Add broken energy computation and a pretty picture

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,34 @@
 use std::env;
 
-use image::{DynamicImage, RgbImage};
+use image::{DynamicImage, RgbImage, ColorType};
 
 fn energy(img: &RgbImage, x: u32, y: u32) -> u32 {
-    let (w, h) = img.dimensions();
+    // let (w, h) = img.dimensions();
 
-    let h1 = (x - 1).max(0);
-    let h2 = (x + 1).min(w - 1);
-    let v1 = (y - 1).max(0);
-    let v2 = (y + 1).min(h - 1);
+    // let h1 = if x == 0 { 0 } else { x - 1 };
+    // let h2 = (x + 1).min(w - 1);
+    // let v1 = if y == 0 { 0 } else { y - 1 };
+    // let v2 = (y + 1).min(h - 1);
 
-    let h1 = &img.get_pixel((h1), y).data;
-    let h2 = &img.get_pixel(h2, y).data;
-    let v1 = &img.get_pixel(x, v1).data;
-    let v2 = &img.get_pixel(x, v2).data;
+    // let h1 = &img.get_pixel(h1, y).data;
+    // let h2 = &img.get_pixel(h2, y).data;
+    // let v1 = &img.get_pixel(x, v1).data;
+    // let v2 = &img.get_pixel(x, v2).data;
 
-    let mut dx = 0;
-    let mut dy = 0;
+    // let mut dx = 0;
+    // let mut dy = 0;
 
-    for i in 0..3 {
-        let dh = u32::from(h1[i]-h2[i]);
-        let dv = u32::from(v1[i]-v2[i]);
-        dx += dh;
-        dy += dv;
-    }
+    // for i in 0..3 {
+    //     let dh = (h1[i] as i16) - (h2[i] as i16);
+    //     let dv = (v1[i] as i16) - (v2[i] as i16);
 
-    return dx + dy;
+    //     dx += (dh * dh) as u32;
+    //     dy += (dv * dv) as u32;
+    // }
+
+    // return dx + dy;
+
+    return if x % 25 == 0 { 1 } else { 0 };
 }
 
 fn main() {
@@ -37,4 +40,26 @@ fn main() {
     } else {
         img.to_rgb()
     };
+
+    let mut cur_min = energy(&img_buf, 0, 0);
+    let mut cur_max = cur_min;
+    let e_buf: Vec<f32> = img_buf.enumerate_pixels()
+        .map(|(x, y, _)| {
+            let e = energy(&img_buf, x, y);
+            cur_min = cur_min.min(e);
+            cur_max = cur_max.max(e);
+            e as f32
+        })
+        .collect();
+    
+    let range = (cur_max - cur_min) as f32;
+    let output: Vec<u8> = e_buf.into_iter()
+        .map(|e| (255.0 * (e - (cur_min as f32)) / range) as u8)
+        .collect();
+    
+    image::save_buffer(
+        "energy.png", &output,
+        img_buf.width(), img_buf.height(),
+        ColorType::Gray(8)
+    ).unwrap();
 }
